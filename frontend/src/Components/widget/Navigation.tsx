48f59cd0c4be1704546bdb2afacb89296d47657c
Daniel <danielkibret8@gmail.com>
2025-06-13 23:24:10 +0300

    change

--- frontend/src/Components/widget/Navigation.tsx
+++ frontend/src/Components/widget/Navigation.tsx
@@ -26,7 +26,6 @@ const Navbar = () => {
       <div className="container mx-auto px-4">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
-            {/* Mobile menu button */}
             <button
               className="md:hidden p-2 text-gray-500 hover:text-gray-700 focus:outline-none"
               onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
