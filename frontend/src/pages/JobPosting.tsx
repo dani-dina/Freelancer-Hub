4966590fd24810a727685c5c1be2904adced1665
Daniel <danielkibret8@gmail.com>
2025-07-16 23:31:56 +0300

    change

--- frontend/src/pages/JobPosting.tsx
+++ frontend/src/pages/JobPosting.tsx
@@ -101,7 +101,7 @@ const JobPostingPage = () => {
               ></textarea>
             </div>
 
-            {/* Job Details section*/}
+            {/* Job Details sections */}
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-6">
               {/* Job Type sections */}
               <div>
