32b1b1f08b63c321b300be42af2bc3d939c63eab
Daniel <danielkibret8@gmail.com>
2025-07-16 23:32:06 +0300

    change

--- frontend/src/pages/JobPosting.tsx
+++ frontend/src/pages/JobPosting.tsx
@@ -84,7 +84,7 @@ const JobPostingPage = () => {
               />
             </div>
 
-            {/* Job Description section */}
+            {/* Job Description sections */}
             <div className="mb-6">
               <label htmlFor="description" className="block text-sm font-medium text-gray-700 mb-1">
                 Job Description *
