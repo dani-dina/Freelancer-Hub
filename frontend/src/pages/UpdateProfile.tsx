f8d04cdbf3ec3b862cd9ac83423dda21cfb79318
Daniel <danielkibret8@gmail.com>
2025-08-04 23:55:41 +0300

    change

--- frontend/src/pages/UpdateProfile.tsx
+++ frontend/src/pages/UpdateProfile.tsx
@@ -102,7 +102,7 @@ const UpdateProfilePage = () => {
         </div>
       </header>
 
-      {/* Main Contents */}
+      {/* Main Content */}
       <main className="container mx-auto px-4 py-8">
         <div className="max-w-6xl mx-auto">
           {/* Profile Header*/}
